48ce19fd4d7a3e1776a72bbff4804eeb19f3eb9e
z9in <113665653+z9in@users.noreply.github.com>
2022-11-17 23:18:16 +0900

    제품정보(sub_3_0_product) 자바스크립트 수정
    * sub_3_0.js 수정
    - 검색기능 추가
    - 분류별 제품 표시 기능 추가
    - 검색 결과 페이지 넘기기 기능 추가

--- js/sub_3_0.js
+++ js/sub_3_0.js
@@ -1,5 +1,74 @@
 // sub_3_0.js
 $(function(){
+    
+    $('#kind ul li').eq(0).on('click',all);
+    function all() {
+        $('.viewBox ul').css({
+            top:'0'
+        });
+        $('.viewBox ul li').css({
+            display : 'block'
+        });
+        $('.dial .numbers p').css('display','block')
+    }
+
+    $('#kind ul li').eq(1).on('click',car_out)
+
+    function car_out() {
+        $('.viewBox ul').css({
+            top:'0'
+        });
+        $('.viewBox .container ul').css('height','2800px')
+        $('.viewBox ul li').css({
+            display: 'none'
+        });
+        $('.viewBox ul .car_out').css({
+            display: 'block'
+        });
+        $('.viewBox ul .all').eq(0).css({
+            display : 'block'
+        });
+        $('.dial .numbers p').css('display','block')
+        $('.dial .numbers p').eq(2).css('display','none')
+        $('.dial .numbers p').eq(3).css('display','none')
+    }
+
+    $('#kind ul li').eq(2).on('click',car_in)
+    function car_in() {
+        $('.viewBox ul').css({
+            top:'0'
+        });
+        $('.viewBox .container ul').css('height','1360px');
+        $('.viewBox ul li').css({
+            display: 'none'
+        });
+        $('.viewBox ul .car_in').css({
+            display : 'block',
+        })
+        $('.dial .numbers p').css('display','none');
+        $('.dial .numbers p').eq(0).css('display','block');
+    }
+
+    $('#kind ul li').eq(3).on('click',ect)
+    function ect() {
+        $('.viewBox ul').css({
+            top:'0'
+        });
+        $('.viewBox .container ul').css('height','1360px');
+        $('.viewBox ul li').css({
+            display: 'none'
+        });
+        $('.viewBox ul .ect').css({
+            display : 'block'
+        })
+        $('.viewBox ul .all').eq(0).css({
+            display : 'block'
+        })
+        $('.dial .numbers p').css('display','none');
+        $('.dial .numbers p').eq(0).css('display','block');
+    }
+
+    //다이얼
     $('.dial .numbers p').eq(0).on('click',()=>
     $('.viewBox ul').css({
         position : 'absolute',
@@ -23,49 +92,36 @@ $(function(){
         position : 'absolute',
         top : '-4080px'
     })
-    )
+    )    
+    //검색 
+    let itemArr = [];
+    for(i=0;i<23;i++) {
+        itemArr.push($('.viewBox ul h2').eq(`${i}`).text().toLowerCase());
+    }
+    console.log(itemArr[0]);
+    $('.search').ons('click',search)
+    function search(){
+        let result = itemArr.indexOf($('.textbox').val().toLowerCase());
+        console.log(result);
 
-    $('#kind ul li').eq(0).on('click',() =>
-    $('.viewBox ul li').css({
-        display : 'block',
-        margin : '40px 0px',
-    })
-    )
-    $('#kind ul li').eq(1).on('click',car_out)
-    function car_out() {
-        $('viewBox ul').css({
-            height : '4000px'
-        })
-        $('.viewBox ul li').css({
-            display: 'none',
-            margin : '0px'
+        $('.viewBox ul').css({
+            top:'0'
         });
-        $('.viewBox ul .car_out').css({
-            display : 'block',
-            margin : '20px 0px'
-        })
-    }
-    $('#kind ul li').eq(2).on('click',car_in)
-    function car_in() {
+        $('.viewBox .container ul').css('height','1360px');
         $('.viewBox ul li').css({
-            display: 'none',
-            margin : '0'
-        });
-        $('.viewBox ul .car_in').css({
-            display : 'block',
-            margin : '20px 0px'
-        })
-    }
-    $('#kind ul li').eq(3).on('click',ect)
-    function ect() {
-        $('.viewBox ul .car_out').css({
-            margin : '0',
             display: 'none'
         });
-        $('.viewBox ul .car_in').css({
-            margin : '0',
-            display : 'none'
+        $('.viewBox ul li').eq(result).css({
+            display : 'block'
         })
+        $('.viewBox ul .all').css({
+            display : 'block'
+        })
+        $('.dial .numbers p').css('display','none');
+        $('.dial .numbers p').eq(0).css('display','block');
     }
+        
+
+    
     
 })
