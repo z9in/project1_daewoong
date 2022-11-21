6aabd24a84b112f6040872c454803f15a2c569bc
z9in <113665653+z9in@users.noreply.github.com>
2022-11-21 09:36:10 +0900

    sub_2 오류수정

--- js/sub_2_0.js
+++ js/sub_2_0.js
@@ -16,7 +16,7 @@ let imageEl = document.getElementsByClassName('images')[0];
 
 let varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
  
-if ( varUA.indexOf('android') > -1) {
+if (screen.width < 768 ) {
     btnEl.addEventListener('click',() => paperEl.style.display='block');
 
     closeEl.addEventListener('click',() => paperEl.style.display='none');
@@ -33,24 +33,7 @@ function translate() {
             display: 'block'    
         })
 }
-} else if ( varUA.indexOf("iphone") > -1 || varUA.indexOf("ipad") > -1 ||varUA.indexOf("ipod") > -1 ) {
-    btnEl.addEventListener('click',() => paperEl.style.display='block');
-
-    closeEl.addEventListener('click',() => paperEl.style.display='none');
-    
-    let num = 0;
-imageEl.addEventListener('click',translate);
-function translate() {
-        num++
-        let numb = num%14
-        $('#paper .images img').css({
-            display: 'none'    
-        })
-        $('#paper .images img').eq(numb).css({
-            display: 'block'    
-        })
-}
-} else {
+} else if (screen.width > 768) {
     btnEl.addEventListener('click',() => paperEl.style.display='block');
 
 closeEl.addEventListener('click',() => paperEl.style.display='none');
